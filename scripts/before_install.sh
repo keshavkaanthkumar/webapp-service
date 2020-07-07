06e7da42d34ccd6530f79e7ec7d2665345fe1875
Keshav Kaanth Kumar <kumar.ke@husky.neu.edu>
2020-07-06 20:44:26 -0400

    Added systemd

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 export app_root=/home/ubuntu/webapp
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+cd /home/ubuntu/webapp
+sudo chown tomcat:tomcat webapp-0.0.1-SNAPSHOT.jar
+sudo chmod 500 webapp-0.0.1-SNAPSHOT.jar
+sudo systemctl daemon-reload
+sudo systemctl enable tomcat.service
 sudo systemctl start tomcat.service
+sudo systemctl status tomcat
